3ca67ba408f2419c162ebbfcfd9bb1633167f58d
Rashadul Haque <mohammed.haque@iths.se>
2022-12-17 00:13:06 +0100

    Started tests for getContactById

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -10,6 +10,7 @@ import makeApp from "../app";
 
 const createContact = jest.fn(); //Test done
 const getAllContact = jest.fn(); //Test done
+const getContactById = jest.fn(); //Test in progress
 
 const app = makeApp({ createContact, getAllContact });
 
@@ -89,6 +90,45 @@ describe("GET /contact", () => {
     getAllContact.mockResolvedValue([]);
     const response = await request(app).get("/contact");
     console.log(response.body); // it should be an empty object
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+// *******************Get contact by id ********************************************************
+describe("GET/contact/:id", () => {
+  it("should return 200 when contact with valid id does exist ", async () => {
+    const response = await request(app).get(
+      "/contact/639774eff665a1529732c4f0"
+    );
+    expect(response.statusCode).toBe(200);
+  });
+  it("should return combined data (contact and geocodes) with correct value", async () => {
+    const response = await request(app).get(
+      "/contact/639774eff665a1529732c4f0"
+    );
+    expect(response.body).toStrictEqual({
+      firstname: "Anna",
+      lastname: "Andersson",
+      email: "anna.andersson@gmail.com",
+      personalnumber: "550713-1405",
+      address: "Utvecklargatan 12",
+      zipCode: "111 22",
+      city: "Stockholm",
+      country: "Sweden",
+      latitude: 51.5073219,
+      longitude: -0.1276474,
+    });
+  });
+
+  it("should return 400 when id does not exist or invalid", async () => {
+    const response = await request(app).get("/contact/invalid");
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return 404 when contact does not exist with query id", async () => {
+    const response = await request(app).get(
+      "/contact/639774eff665a15297nodata"
+    );
     expect(response.statusCode).toBe(404);
   });
 });
